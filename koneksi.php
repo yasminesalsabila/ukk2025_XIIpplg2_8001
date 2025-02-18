522386efcacca04b38f303c1def850389e40e3b1
yasminesalsabila <yasminsalsabila1406@gmail.com>
2025-02-18 07:43:24 +0700

    tes 2

--- koneksi.php
+++ koneksi.php
@@ -2,4 +2,10 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-$koneksi = mysqli_connect('localhost','root','','ukk2025_XIIpplg2_8001');
+
+$koneksi = mysqli_connect('localhost', 'root', '', 'ukk2025_xiipplg2_8001');
+
+if (!$koneksi) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+?>
